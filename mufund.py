0b1cb2d1507a0e9c5506fcb6955fe5bba2f9aea8
Evan Laske <evan.laske@gmail.com>
2014-03-02 00:25:23 -0500

    Created a class that handles the stock quote info.

--- mufund.py
+++ mufund.py
@@ -3,55 +3,27 @@
 # @Author: elaske
 # @Date:   2014-03-01 21:45:31
 # @Last Modified by:   Evan Laske
-# @Last Modified time: 2014-03-01 22:56:45
+# @Last Modified time: 2014-03-02 00:22:36
 
 import urllib
 import urllib2
 from bs4 import BeautifulSoup
 import html5lib
 import re
+from stockquote import StockQuote
 
 def main():
-	ticker = "FBIOX"
+    ticker = "FBIOX"
 
-	quoteURL = 'http://quotes.morningstar.com/fund/f?t='
-	portfolioURL = 'http://portfolios.morningstar.com/fund/summary?t='
-	holdingsURL = 'http://portfolios.morningstar.com/fund/holdings?t='
-	googleFinanceURL = 'http://www.google.com/finance?q='
+    quoteURL = 'http://quotes.morningstar.com/fund/f?t='
+    portfolioURL = 'http://portfolios.morningstar.com/fund/summary?t='
+    holdingsURL = 'http://portfolios.morningstar.com/fund/holdings?t='
+    googleFinanceURL = 'http://www.google.com/finance?q='
 
-	print get_quote("goog")
-
-def get_quote(symbol):
-    base_url = 'http://finance.google.com/finance?q='
-    content = urllib.urlopen(base_url + symbol).read()
-
-    return [get_price(content), get_change(content), get_pct_change(content)]
-
-def get_price(content):
-    m = re.search('id="ref_[0-9]*_l".*?>(.*?)<', content)
-    if m:
-        price = m.group(1)
-    else:
-        price = 'no quote available for symbol'
-    return price
-
-def get_change(content):
-    m = re.search('id="ref_[0-9]*_c".*?>(.*?)<', content)
-    if m:
-        change = m.group(1)
-    else:
-        change = 'no quote available for symbol'
-    return change
-
-def get_pct_change(content):
-    m = re.search('id="ref_[0-9]*_cp".*?>(.*?)<', content)
-    if m:
-    	# Remove excess characters to make it a float.
-        pct_change = m.group(1).strip('()%')
-    else:
-        pct_change = 'no quote available for symbol'
-    return pct_change
+    sq = StockQuote("goog")
+    print sq.price, sq.change, sq.percent
+    print sq
 
 # Standard main call
 if __name__ == "__main__":
-	main()
+    main()
---
+++ stockquote.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Evan Laske
+# @Date:   2014-03-01 23:12:45
+# @Last Modified by:   Evan Laske
+# @Last Modified time: 2014-03-02 00:23:39
+
+import urllib
+import re
+
+class StockQuote:
+    def __init__(self):
+        self._url = 'http://finance.google.com/finance?q='
+        self._ticker = ''
+
+    def __init__(self, ticker):
+        self._url = 'http://finance.google.com/finance?q='
+        self._ticker = ticker
+        # Grab the content since the requirements are here.
+        self.update()
+
+    def url():
+        doc = "The URL that the quotes will be retrieved from."
+        def fget(self):
+            return self._url
+        def fset(self, value):
+            self._url = value
+            # Grab the content from the new URL.
+            self.update()
+        def fdel(self):
+            del self._url
+        return locals()
+    url = property(**url())
+
+    def ticker():
+        doc = "The ticker of the data to be collected."
+        def fget(self):
+            return self._ticker
+        def fset(self, value):
+            self._ticker = value
+            # Grab the content from the new ticker.
+            self.update()
+        def fdel(self):
+            del self._ticker
+        return locals()
+    ticker = property(**ticker())
+
+    def price():
+        doc = "The price of the given ticker."
+        def fget(self):
+            m = re.search('id="ref_[0-9]*_l".*?>(.*?)<', self._content)
+            if m:
+                price = m.group(1)
+            else:
+                price = 'No quote available for symbol'
+            return price
+        # No setter
+        # No deleter 
+        return locals()
+    price = property(**price())
+
+    def change():
+        doc = "The change in price in absolute terms."
+        def fget(self):
+            m = re.search('id="ref_[0-9]*_c".*?>(.*?)<', self._content)
+            if m:
+                change = m.group(1)
+            else:
+                change = 'No quote available for symbol'
+            return change
+        # No setter
+        # No deleter 
+        return locals()
+    change = property(**change())
+
+    def percent():
+        doc = "The percent change in price."
+        def fget(self):
+            m = re.search('id="ref_[0-9]*_cp".*?>(.*?)<', self._content)
+            if m:
+                percent = m.group(1).strip('()%')
+            else:
+                percent = 'No quote available for symbol'
+            return percent
+        # No setter
+        # No deleter 
+        return locals()
+    percent = property(**percent())
+
+    def update(self):
+        """
+        Collect new data from the source URL and ticker.
+        """
+        # Grab the content from the new URL.
+        self._content = urllib.urlopen(self._url + self._ticker).read()
+
