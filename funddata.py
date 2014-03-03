1742f56bdbf18ff30ae786c30e8f677fc1423578
Evan Laske <evan.laske@gmail.com>
2014-03-02 19:40:37 -0500

    Gathers the raw data from Morningstar by learning from one of their scripts.

--- funddata.py
+++ funddata.py
@@ -3,13 +3,14 @@
 # @Author: Evan Laske
 # @Date:   2014-03-02 01:05:31
 # @Last Modified by:   Evan Laske
-# @Last Modified time: 2014-03-02 02:31:27
+# @Last Modified time: 2014-03-02 19:38:32
 
 import urllib
 import urllib2
 import html5lib
 from bs4 import BeautifulSoup
 from stockquote import StockQuote
+import json
 
 class MutualFundData(StockQuote):
     """
@@ -33,6 +34,26 @@ class MutualFundData(StockQuote):
         """
         # First, update the quote for the fund through StockQuote
         super(MutualFundData, self).update()
+
+        # These URLs are from the Morningstar site scripts.
+        ajaxURL = 'http://portfolios.morningstar.com/fund/ajax/'
+        ajaxPage = 'holdings_tab?&t=XNAS:FBIOX&region=usa&culture=en-US&cur=USD&dataType=0&callback=?'
+        # Grab the JSON data from the source
+        jsonHoldings = urllib.urlopen(ajaxURL + ajaxPage).read()
+        # Strip the leading and trailing characters so we can load it correctly.
+        jsonHoldings = jsonHoldings.strip('?()')
+
+        dictHoldings = json.loads(jsonHoldings)
+
+        print dictHoldings.keys()
+
+        # Create a soup for the HTML
+        self._soup = BeautifulSoup(dictHoldings['htmlStr'])
+        self._holding_data = self._soup('table', id='equity_holding_tab')
+        self._price_data = self._soup('table', id='equityPrice_holding_tab')
+
+        print self._holding_data
+        print self._price_data
 
     def holdings(self):
         """
