b34cf8dcee9dae3357560241c2cc539990030d44
Evan Laske <evan.laske@gmail.com>
2014-03-06 22:44:41 -0500

    Added the return of holdings()

--- funddata.py
+++ funddata.py
@@ -3,7 +3,7 @@
 # @Author: Evan Laske
 # @Date:   2014-03-02 01:05:31
 # @Last Modified by:   Evan Laske
-# @Last Modified time: 2014-03-06 22:25:31
+# @Last Modified time: 2014-03-06 22:43:57
 
 import urllib
 import urllib2
@@ -80,36 +80,36 @@ class MutualFundData(StockQuote):
         # Join the sub-list strings with spaces to get a list of combined strings
         holdingHeaderStrings = [' '.join(i) for i in holdingHeaderStrings]
 
-        print "List:"
-        print holdingHeaderList
-        print "Strings:"
-        print holdingHeaderStrings
+        # print "List:"
+        # print holdingHeaderList
+        # print "Strings:"
+        # print holdingHeaderStrings
 
         # Remove all the extra whitespace from any of these strings.
         holdingHeaderStrings = [' '.join(s.split()) for s in holdingHeaderStrings]
 
-        print "Split / Join:"
-        print holdingHeaderStrings
+        # print "Split / Join:"
+        # print holdingHeaderStrings
 
-        print "Contents:"
+        # print "Contents:"
         #print '\nholdingRows'
         #print holdingRows
         #print holdingRows[0]
-        print holdingRows[0].contents
+        # print holdingRows[0].contents
 
         # Remove all of the non-tags to remove extranneous objects.
         holdingRowList = [filter(lambda x: isinstance(x, element.Tag), e.contents) for e in holdingRows]
-        print holdingRowList[0], len(holdingRowList[0])
+        # print holdingRowList[0], len(holdingRowList[0])
         # Remove the extra <td> that doesn't match up with the <th> header elements
         for r in holdingRowList:
             del r[3]
-        print holdingRowList[0], len(holdingRowList[0])
+        # print holdingRowList[0], len(holdingRowList[0])
 
-        print '\nStrings:'
+        # print '\nStrings:'
         # For each row's elements, get the only string from it. 
         # If there so happens to be more than one, it will combine them.
         holdingRowStrings = [[' '.join([s for s in e.strings]) for e in row] for row in holdingRowList]
-        print holdingRowStrings
+        # print holdingRowStrings
 
         # Put the holding data into a dictionary
         holdingData = OrderedDict()
@@ -128,8 +128,8 @@ class MutualFundData(StockQuote):
             # Use the name of the holding as the key for this whole thing.
             holdingData[row[1]] = temp
 
-        print '\nData:'
-        print holdingData
+        # print '\nData:'
+        # print holdingData
 
         # --------------------- PRICE DATA TABLE ---------------------
 
@@ -140,10 +140,10 @@ class MutualFundData(StockQuote):
         holdingRows = tbody[0]('tr', class_='')
         # Remove all of the non-tags to remove extranneous objects.
         holdingRowList = [filter(lambda x: isinstance(x, element.Tag), e.contents) for e in holdingRows]
-        print "\nPrice Row:"
-        print holdingRowList[0], len(holdingRowList[0])
+        # print "\nPrice Row:"
+        # print holdingRowList[0], len(holdingRowList[0])
 
-        print '\nStrings:'
+        # print '\nStrings:'
         # For each row's elements, get the only string from it. 
         # If there so happens to be more than one, it will combine them.
         holdingRowStrings = [[' '.join([s for s in e.strings]) for e in row] for row in holdingRowList]
@@ -151,7 +151,7 @@ class MutualFundData(StockQuote):
         holdingRowStrings = [[' '.join(s.split()) for s in l] for l in holdingRowStrings]
         # Filter out empty strings - they're falsy
         holdingRowStrings = [filter(lambda x: x, e) for e in holdingRowStrings]
-        print holdingRowStrings
+        # print holdingRowStrings
 
         # Within the only <table> and within the only <thead>, get all of the header cells.
         holdingHeaderTags = self._price_data[0]('thead')[0]('th')
@@ -167,8 +167,8 @@ class MutualFundData(StockQuote):
         # Filter out empty strings - they're falsy
         holdingHeaderStrings = filter(lambda x: x, holdingHeaderStrings)
 
-        print "\nHeader:"
-        print holdingHeaderStrings, len(holdingHeaderStrings)
+        # print "\nHeader:"
+        # print holdingHeaderStrings, len(holdingHeaderStrings)
 
         # Using an OrderedDict to keep the sorting order from the website.
         for row in holdingRowStrings:
@@ -177,9 +177,9 @@ class MutualFundData(StockQuote):
                 # Add it to the existing data - it will match by the name (element 1)
                 holdingData[row[0]][holdingHeaderStrings[index]] = row[index]
 
-        print '\nData Header:'
-        # Print the holding data keys to compare.
-        for holding in holdingData.keys():
-            print holdingData[holding].keys()
+        # print '\nData Header:'
+        # # Print the holding data keys to compare.
+        # for holding in holdingData.keys():
+        #     print holdingData[holding].keys()
 
-        
+        return [{v['Ticker'], v['% Portfolio Weight']} for (k, v) in holdingData.items()]
