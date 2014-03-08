a2e97cb41e5ffb5f97484e50385f93dbece1f743
Evan Laske <evan.laske@gmail.com>
2014-03-07 21:37:17 -0500

    Redid stockquote properties to be in 2.x+ form or above. Close #4. Close #2 because this is upgraded now, too.

--- stockquote.py
+++ stockquote.py
@@ -3,7 +3,7 @@
 # @Author: Evan Laske
 # @Date:   2014-03-01 23:12:45
 # @Last Modified by:   Evan Laske
-# @Last Modified time: 2014-03-07 01:43:37
+# @Last Modified time: 2014-03-07 21:35:20
 
 import urllib
 import re
@@ -25,58 +25,46 @@ class StockQuote:
         # Grab the content since the requirements are here.
         self.update()
 
-    def url():
-        doc = "The URL that the quotes will be retrieved from."
-        def fget(self):
-            return self._url
-        def fset(self, value):
-            self._url = value
-            # Grab the content from the new URL.
-            self.update()
-        def fdel(self):
-            del self._url
-        return locals()
-    url = property(**url())
+    @property
+    def url(self):
+        """The URL that the quotes will be retrieved from."""
+        return self._url
+    @url.setter
+    def url(self, value):
+        self._url = value
+        # Grab the content from the new URL.
+        self.update()
+    @url.deleter
+    def url(self):
+        del self._url
 
-    def ticker():
-        doc = "The ticker of the data to be collected."
-        def fget(self):
-            return self._data['tickerSymbol']
-        def fset(self, value):
-            self._ticker = value
-            # Grab the content from the new ticker.
-            self.update()
-        def fdel(self):
-            del self._ticker
-        return locals()
-    ticker = property(**ticker())
+    @property
+    def ticker(self):
+        """The ticker of the data to be collected."""
+        return self._data['tickerSymbol']
+    @ticker.setter
+    def ticker(self, value):
+        self._ticker = value
+        # Grab the content from the new ticker.
+        self.update()
+    @ticker.deleter
+    def ticker(self):
+        del self._ticker
 
-    def price():
-        doc = "The price of the given ticker."
-        def fget(self):
-            return self._data['price']
-        # No setter
-        # No deleter 
-        return locals()
-    price = property(**price())
+    @property
+    def price(self):
+        """The price of the given ticker."""
+        return self._data['price']
 
-    def change():
-        doc = "The change in price in absolute terms."
-        def fget(self):
-            return self._data['priceChange']
-        # No setter
-        # No deleter 
-        return locals()
-    change = property(**change())
+    @property
+    def change(self):
+        """The change in price in absolute terms."""
+        return self._data['priceChange']
 
-    def percent():
-        doc = "The percent change in price."
-        def fget(self):
-            return self._data['priceChangePercent']
-        # No setter
-        # No deleter 
-        return locals()
-    percent = property(**percent())
+    @property
+    def percent(self):
+        """The percent change in price."""
+        return self._data['priceChangePercent']
 
     def update(self):
         """
@@ -85,12 +73,8 @@ class StockQuote:
         # Grab the content from the new URL.
         self._content = urllib.urlopen(self._url + self._ticker).read()
         self._soup = BeautifulSoup(self._content)
-        # temp_data = self._soup('div', id="sharebox-data")[0]
-        # for meta in temp_data.contents:
-        #     print meta, type(meta), meta.name
         # Get strip all of the meta tag attributes into a dictionary from the correct div tag container.
         self._data = {
             meta.get('itemprop'): meta.get('content') 
             for meta in self._soup('div', id="sharebox-data")[0].find_all('meta')
         }
-        #print self._data
